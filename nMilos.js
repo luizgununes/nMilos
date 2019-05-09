683dc1c80e237d161ad60c5d8a023bae5d40e434
Luiz Gustavo Nunes <gu-nunes@hotmail.com>
2019-05-09 17:09:55 -0300

    beta  v1

---
+++ nMilos.js
@@ -0,0 +1,79 @@
+(function ($) {
+
+    var self = $.nMilos = new function () { };
+
+    $.extend(self, {
+
+        nMilosBackgrounds: [
+            'https://i.imgur.com/DMwAuUT.png'
+        ],
+
+        nMilosImgs: [
+            'https://i.imgur.com/u41b6Eg.jpg',
+            'https://i.imgur.com/2MARnbC.jpg',
+            'https://i.imgur.com/DMwAuUT.png',
+            'https://i.imgur.com/grNEQbS.jpg',
+            'https://i.imgur.com/kIcijvP.jpg'
+        ],
+
+        handleImages: function (lstImgs, time) {
+            $.each($('img'), function (i, item) {
+                //Skip if image is already replaced
+                if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                    var h = $(item).height();
+                    var w = $(item).width();
+
+                    //If image loaded
+                    if (h > 0 && w > 0) {
+                        //Replace
+                        $(item).css('width', w + 'px').css('height', h + 'px');
+                        $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+                    }
+                    else {
+                        //Replace when loaded
+                        $(item).load(function () {
+                            //Prevent 'infinite' loop
+                            if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                                var h = $(item).height();
+                                var w = $(item).width();
+                                $(item).css('width', w + 'px').css('height', h + 'px');
+                                $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+                            }
+                        });
+                    }
+                }
+            });
+
+            //Keep replacing
+            if (time > 0) {
+                setTimeout(function () { self.handleImages(lstImgs, time); }, time);
+            }
+        },
+
+        handleLogo: function (bgImgs, time) {
+            $backgroundImages = $(
+                '[class*=logo], [class*=header], [id*=header], [id*=logo],' +
+                '[class*=logo] span, [class*=header] span, [id*=header] span, [id*=logo] span,' +
+                '[class*=logo] h1, [class*=header] h1, [id*=header] h1, [id*=logo] h1,' +
+                '[class*=logo] a, [class*=header] a, [id*=header] a, [id*=logo] a'
+            )
+                .filter(function () {
+                    backgroundImg = $(this).css('background-image');
+                    return backgroundImg && backgroundImg != 'none';
+                }
+                );
+
+            $backgroundImages.each(function (i, item) {
+                $(item).css('background-image', 'url(' + bgImgs[Math.floor(Math.random() * bgImgs.length)] + ')');
+                $(item).css('background-position', '0 0');
+                $(item).css('background-repeat', 'no-repeat');
+                $(item).css('background-size', 'contain');
+            });
+        }
+    });
+
+    $(function () {
+        self.handleImages(self.nMilosImgs, 3000);
+        self.handleLogo(self.nMilosBackgrounds, 3000);
+    });
+})(jQuery);
